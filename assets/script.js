ffb32cbe378f785f7abebc5f339d2004dc5179e0
Mariam Dib <mariame.dibaga@gmail.com>
2024-02-16 12:16:25 +0100

    Validation des boucles infini nettoyage index

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ const slides = [
     image: "slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
- ];
+];
 
 //Recuperation de mes flèches à partir de mon HTML//
 //récuperation de mes slides//
@@ -25,45 +25,77 @@ const rightArrow = document.querySelector(".arrow_right");
 
 const imageSlide = document.querySelector(".banner-img");
 const tagLine = document.querySelector("#banner p");
-//const baseUrl = "./assets/images/slideshow";A verifier//
+const baseUrl = "./assets/images/slideshow/";
 let currentSlideIndex = 0;
 
 //Récuperation élément html Variable pour les dots//
 const dotsContainer = document.querySelector(".dots");
 
-//Ajout des bullet point dynamique//
-slides.forEach((slide) => {
-  slide.bullet = document.createElement("span");
-  slide.bulletPoint.classList.add("dot  dotsContainer.appendChild(slide.bulletPoint);
-});
+//tous les dots du DOM
+let allDots;
 
-slides[0].bulletPoint.classList.add("dot_selected");
+console.log(allDots)
 
+function main() {
+  initDots();
+  prevSlide();
+  nextSlide();
+
+
+}
+
+main();
+
+function initDots() {
+  //Ajout des bullet point dynamique//
+  slides.forEach((slide) => {
+    //console.log(slide)
+    const bulletPoint = document.createElement("span");
+    bulletPoint.classList.add("dot");
+    dotsContainer.appendChild(bulletPoint);
+  });
+
+
+
+  allDots = document.querySelectorAll(".dots .dot")
+  //premier dots en selected
+  allDots[currentSlideIndex].classList.add("dot_selected");
+
+}
 //Attachement des eventlistener aux flèches//
 
-leftArrow.addEventListener("click", function () {
-	slides[currentSlideIndex].bulletPoint.classList.remove("dot_selected");
-	currentSlideIndex--;
-	if (currentSlideIndex == -1) {
-	  currentSlideIndex = slides.length - 1;	
-    console.log("J'ai cliqué sur la flèche gauche");
-	}
-  //Mise à jour de l'image et du tagline selon la diapositive//
-	imageSlide.src =
-	  "./assets/images/slideshow/" + slides[currentSlideIndex].image;
-	tagLine.innerHTML = slides[currentSlideIndex].tagLine;
-	slides[currentSlideIndex].bulletPoint.classList.add("dot_selected");
-});
 
-rightArrow.addEventListener("click", function () {
-  slides[currentSlideIndex].bulletPoint.classList.remove("dot_selected");
-  currentSlideIndex++;
-  if (currentSlideIndex == slides.length) {
-	currentSlideIndex = 0	
-  console.log("Jai cliqué sur la flèche droite");
-  }
-  imageSlide.src =
-    "./assets/images/slideshow/" + slides[currentSlideIndex].image;
-  tagLine.innerHTML = slides[currentSlideIndex].tagLine;
-  slides[currentSlideIndex].bulletPoint.classList.add("dot_selected");
-});
+function prevSlide() {
+  leftArrow.addEventListener("click", function () {
+
+    //tous les dots du DOM
+    allDots[currentSlideIndex].classList.remove("dot_selected");
+
+    currentSlideIndex--;
+    if (currentSlideIndex == -1) {
+      currentSlideIndex = slides.length - 1;
+      console.log("J'ai cliqué sur la flèche gauche");
+    }
+    //Mise à jour de l'image et du tagline selon la diapositive//
+    imageSlide.src = baseUrl + slides[currentSlideIndex].image;
+    tagLine.innerHTML = slides[currentSlideIndex].tagLine;
+    allDots[currentSlideIndex].classList.add("dot_selected");
+  });
+
+}
+
+
+
+function nextSlide() {
+  rightArrow.addEventListener("click", function () {
+    allDots[currentSlideIndex].classList.remove("dot_selected");
+    currentSlideIndex++;
+    if (currentSlideIndex == slides.length) {
+      currentSlideIndex = 0
+      console.log("Jai cliqué sur la flèche droite");
+    }
+    imageSlide.src = baseUrl + slides[currentSlideIndex].image;
+    tagLine.innerHTML = slides[currentSlideIndex].tagLine;
+    allDots[currentSlideIndex].classList.add("dot_selected");
+  });
+}
